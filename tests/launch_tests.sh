7049f1380cb9321048ace62f11d38ca96a3d6fea
Luis Velasco <velascoluis@google.com>
2024-04-30 16:57:18 +0200

    Ammend test launch script

--- tests/launch_tests.sh
+++ tests/launch_tests.sh
@@ -1,3 +1,3 @@
-export PROJECT_ID=velascoluis-dev-sandbox
-export LOCATION=us-central1
+export PROJECT_ID=<TO_DO_DEVELOPER>
+export LOCATION=<TO_DO_DEVELOPER>
 pytest wizard_tests.py --project_id ${PROJECT_ID} --location ${LOCATION}
