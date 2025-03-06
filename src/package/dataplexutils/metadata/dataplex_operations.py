f3d964878b5f6809f254092537783c999e940dd5
Jakub Skuratowicz <jskuratowicz@google.com>
2025-03-06 13:15:18 +0100

    Licensing and cleanup

--- src/package/dataplexutils/metadata/dataplex_operations.py
+++ src/package/dataplexutils/metadata/dataplex_operations.py
@@ -13,9 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-"""Dataplex Utils Metadata Wizard Dataplex operations
-   2024 Google
-"""
+
 # Standard library imports
 import logging
 import toml
