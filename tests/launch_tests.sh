52c2f57eb1fd123537c43909a7d3ea1067439d20
Jakub Skuratowicz <jskuratowicz@google.com>
2025-04-25 09:02:27 +0200

    cleanup and licenses

--- tests/launch_tests.sh
+++ tests/launch_tests.sh
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#export PROJECT_ID="<TO_DO_DEVELOPER>"
-#export LLM_LOCATION="<TO_DO_DEVELOPER>"
-#export DATAPLEX_LOCATION="<TO_DO_DEVELOPER>"
-#export DOCUMENTATION_URI="<TO_DO_DEVELOPER>"
+export PROJECT_ID="<TO_DO_DEVELOPER>"
+export LLM_LOCATION="<TO_DO_DEVELOPER>"
+export DATAPLEX_LOCATION="<TO_DO_DEVELOPER>"
+export DOCUMENTATION_URI="<TO_DO_DEVELOPER>"
 
 
 pytest tests/wizard_tests.py --project_id ${PROJECT_ID} --llm_location ${LLM_LOCATION} --dataplex_location ${DATAPLEX_LOCATION}
